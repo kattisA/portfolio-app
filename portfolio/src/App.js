7a510c9fef112bbc94bedbcc5f1e6ba8c832c58e
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-08-12 17:03:49 +0200

    Starting over with another tutorial/project

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import Presentation from './components/presentation';
-import Sidebar from './components/sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <Sidebar></Sidebar>
-    
+    <div className="App">   
      
      <p>Hello again!</p>
     </div>
