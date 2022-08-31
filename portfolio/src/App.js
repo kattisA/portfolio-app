51df7877a86ec8f291ae9636b2218fb1610114e3
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-08-31 20:07:42 +0200

    Styling the different sections

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,22 @@
 import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Works from "./components/works/Works";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Topbar />
-      <p>Hello again!</p>
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
