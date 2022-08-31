ba578667b5906eb306faaf64e8c209b9f918925e
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-08-31 19:19:01 +0200

    Making new components and topbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,10 @@
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div className="App">   
-     
-     <p>Hello again!</p>
+    <div className="App">
+      <Topbar />
+      <p>Hello again!</p>
     </div>
   );
 }
