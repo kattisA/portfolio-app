4387d99529b98fb372c0e6cd9123c6995c558f6a
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-09-01 18:00:56 +0200

    Styling topbar and adding items

--- portfolio/src/components/portfolio/Portfolio.js
+++ portfolio/src/components/portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import "./portfolio.scss";
 
 function Portfolio() {
-  return <div className="portfolio"></div>;
+  return <div className="portfolio" id="portfolio"></div>;
 }
 
 export default Portfolio;
--- portfolio/src/components/topbar/Topbar.js
+++ portfolio/src/components/topbar/Topbar.js
@@ -1,7 +1,33 @@
 import "./topbar.scss";
+import { GoPerson, GoMail } from "react-icons/go";
 
 function Topbar() {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            kattis.
+          </a>
+          <div className="itemContainer">
+            <GoPerson className="icon" />
+            <span>+46 123 456</span>
+          </div>
+          <div className="itemContainer">
+            <GoMail className="icon" />
+            <span>kattis@mail.se</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Topbar;
