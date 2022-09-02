e4548b0ff1ff2da216339fa03fab0e2b7237ed4a
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-09-02 16:13:03 +0200

    First draft of intro page

--- portfolio/src/components/intro/Intro.js
+++ portfolio/src/components/intro/Intro.js
@@ -1,7 +1,28 @@
 import "./intro.scss";
+import { IoIosArrowDown } from "react-icons/io";
 
 function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/kattis_lejonlid.jpg" alt="lego" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Kattis</h1>
+          <h3>
+            Freelance<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <IoIosArrowDown className="icon" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Intro;
