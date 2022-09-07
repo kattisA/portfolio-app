cbfa8eabd763e2ee096bd10a9ce0069cae6cabce
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2022-09-07 19:55:04 +0200

    Adding new photo and ityped animation

--- portfolio/src/components/intro/Intro.js
+++ portfolio/src/components/intro/Intro.js
@@ -1,12 +1,24 @@
 import "./intro.scss";
 import { IoIosArrowDown } from "react-icons/io";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Designer", "Content Creator"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/Kattis_git.png" alt="lego" />
+          <img src="assets/kattis_git.png" alt="lego" />
         </div>
       </div>
       <div className="right">
@@ -14,7 +26,7 @@ function Intro() {
           <h2>Hi there, I'm</h2>
           <h1>Kattis</h1>
           <h3>
-            Freelance<span></span>
+            Freelance <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
