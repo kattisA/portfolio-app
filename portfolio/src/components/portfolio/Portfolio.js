6ae83523d0ced9bc54e52f28698c206c869923a8
Kattis L <kattis.a@gmail.com>
2022-09-08 17:21:10 +0200

    First draft of portfolio

--- portfolio/src/components/portfolio/Portfolio.js
+++ portfolio/src/components/portfolio/Portfolio.js
@@ -1,7 +1,24 @@
 import "./portfolio.scss";
 
 function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/cookie-the-pom-siNDDi9RpVY-unsplash.jpg" alt="" />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio;
