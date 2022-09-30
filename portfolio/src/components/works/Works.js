f8deb4d140eba0c12516a66742618b685e8da696
Kattis L <kattis.a@gmail.com>
2022-09-30 15:13:10 +0200

    First item on works slider

--- portfolio/src/components/works/Works.js
+++ portfolio/src/components/works/Works.js
@@ -1,7 +1,35 @@
 import "./works.scss";
 
 function Works() {
-  return <div className="works" id="works"></div>;
+  return (
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="Mobile pic" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto eligendi expedita incidunt quas quisquam?
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img
+                src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Works;
