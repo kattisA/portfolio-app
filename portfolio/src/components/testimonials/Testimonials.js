64c983de09420ded46e90ea6de9f2a61e752101f
Kattis L <kattis.a@gmail.com>
2022-10-19 15:25:08 +0200

    Finishing testimonials

--- portfolio/src/components/testimonials/Testimonials.js
+++ portfolio/src/components/testimonials/Testimonials.js
@@ -33,25 +33,20 @@ function Testimonials() {
     <div className="testimonials" id="testimonials">
       <h1>Testimonials</h1>
       <div className="container">
-        <div className="card">
-          <div className="top">
-            <img src="assets/right-arrow.png" className="left" alt="" />
-            <img
-              className="user"
-              src="https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
-            <img className="right" src="assets/youtube.png" alt="" />
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img className="user" src={d.img} alt="" />
+              <img className="right" src={d.icon} alt="" />
+            </div>
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
           </div>
-          <div className="center">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ea
-            quod tempora.
-          </div>
-          <div className="bottom">
-            <h3>Alex</h3>
-            <h4>CEO</h4>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
